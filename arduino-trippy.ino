cb91818b3036d9d097c7aa055c6e532be8281b23
Georg Baumann <baumanngeorg5@gmail.com>
2017-03-18 01:01:43 +0100

    Update arduino-trippy.ino
    changed license from CC4.0 to GPL

--- arduino-trippy.ino
+++ arduino-trippy.ino
@@ -1,9 +1,18 @@
-/***********************************************************************************************************
- * LICENSE                                                                                                 *
- * Dieses Material steht unter der Creative-Commons-Lizenz                                                 *
- * Namensnennung - Nicht kommerziell - Keine Bearbeitungen 4.0 International.                              *
- * Um eine Kopie dieser Lizenz zu sehen, besuchen Sie http://creativecommons.org/licenses/by-nc-nd/4.0/.   *
- ***********************************************************************************************************/
+/*Copyright (C) 2017  Georg Baumann, Berlin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
 //Board Pins
 #define PIN_1 1
 #define PIN_2 2
